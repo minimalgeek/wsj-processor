96a65923be49913ceee18f75e465e7d2d360ccaa
minimalgeek <minimalgeek@gmail.com>
2015-09-02 15:25:48 +0200

    Delete all from target

--- receiver2/target/receiver2/WEB-INF/classes/selects.sql
+++
@@ -1,6 +0,0 @@
-select w.DATE_TIME, w.WORD, w.WORD_COUNT
-from wordcount_view_table as w
-where (
-   select count(*) from wordcount_view_table as w2
-   where w2.DATE_TIME = w.DATE_TIME and w2.WORD_COUNT >= w.WORD_COUNT
-) <= 1000;
