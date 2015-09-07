b2450e9c8322867043d8c094bef15c942cb87dc8
minimalgeek <minimalgeek@gmail.com>
2015-09-07 09:31:03 +0200

    Mongo Aggregate change

---
+++ receiver2/src/main/resources/mongo_article_aggregate.js
@@ -0,0 +1,33 @@
+db.article.aggregate(
+[
+    {$project : {
+        dateDay : {
+            $add : ["$dateDay", 1000*60*60]
+        },
+        articleWords : 1
+    }},
+    {$unwind : "$articleWords"},
+    {$group : 
+        {
+           _id : {year: { $year: "$dateDay" }, month: { $month: "$dateDay" }, day: { $dayOfMonth: "$dateDay" }, word: "$articleWords" },
+	   differentDocuments : {$sum : 1}
+        }
+    },
+    {$project : {
+        _id : 0,
+        word : "$_id.word",
+	year : "$_id.year",
+	month : "$_id.month",
+	day : "$_id.day",
+        differentDocuments : 1
+    }},
+    {$sort : {
+        year : 1,
+        month : 1,
+        day : 1
+    }},
+    {$out : "result"}
+], {
+    allowDiskUse:true,
+    cursor:{}}
+);
---
+++ receiver2/src/main/resources/mongo_article_aggregate_for_test.js
@@ -0,0 +1,31 @@
+db.article.aggregate(
+[
+    {$limit : 300},
+    {$project : {
+        dateDay : {
+            $add : ["$dateDay", 1000*60*60]
+        },
+        articleWords : 1
+    }},
+    {$unwind : "$articleWords"},
+    {$group : 
+        {
+           _id : {year: { $year: "$dateDay" }, month: { $month: "$dateDay" }, day: { $dayOfMonth: "$dateDay" }, word: "$articleWords" },
+	   differentDocuments : {$sum : 1}
+        }
+    },
+    {$project : {
+        _id : 0,
+        word : "$_id.word",
+	year : "$_id.year",
+	month : "$_id.month",
+	day : "$_id.day",
+        differentDocuments : 1
+    }},
+    {$sort : {
+        year : 1,
+        month : 1,
+        day : 1
+    }}
+]
+);
